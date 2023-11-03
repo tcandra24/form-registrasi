e5acb8c510104f5c121437746fde0130bfd30525
Tito Candra <tcandra007@gmail.com>
2023-11-03 16:05:09 +0700

    Rev 03112023

--- resources/views/registrations/index.blade.php
+++ resources/views/registrations/index.blade.php
@@ -21,7 +21,8 @@ Registrasi
             <h5 class="card-title fw-semibold mb-4">Data Anda</h5>
             <div class="alert alert-success alert-dismissible fade show m-2">
                 <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="me-2"><polyline points="9 11 12 14 22 4"></polyline><path d="M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11"></path></svg>
-                <strong>Selamat!</strong> Anda Terdaftar Dalam Acara Fuboru
+                <strong>Selamat!</strong> Anda Terdaftar Dalam Acara Fuboru.
+                Mohon QrCode dan Nomer Registrasi disimpan dengan baik
             </div>
             <div class="d-block w-100 my-4">
                 <div class="d-flex justify-content-center">
