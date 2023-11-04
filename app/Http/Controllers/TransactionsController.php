452acdd53d6482a3ec6b76d3c53bc9ae22f5d6fc
tcandra24 <tcandra007@gmail.com>
2023-11-04 13:58:15 +0700

    Add Transaction Controller

---
+++ app/Http/Controllers/TransactionsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Registration;
+
+class TransactionsController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function edit(Registration $registration)
+    {
+        //
+    }
+
+    public function update(Request $request, Registration $registration)
+    {
+        //
+    }
+}
