f9961af6731791140900839727b3fdebb7b01d96
tcandra24 <tcandra007@gmail.com>
2024-01-23 09:40:00 +0700

    add event count on dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,24 +5,29 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Models\Registration;
+use App\Models\RegistrationMechanic;
 use App\Models\User;
 use App\Models\Shift;
+use App\Models\Event;
 
 class DashboardController extends Controller
 {
     public function index()
     {
         $registration = Registration::count();
+        $registrationMechanic = RegistrationMechanic::count();
         $user = User::count();
         $shift = Shift::count();
+        $event = Event::count();
         $shiftWithQuota = Shift::where('is_active', true)->withCount('registration')->get();
 
         $is_registered = Registration::where('user_id', Auth::user()->id)->exists();
 
         return view('dashboard.index', [
-            'count_registration' => $registration,
+            'count_registration' => $registration + $registrationMechanic,
             'count_user' => $user,
             'count_shift' => $shift,
+            'count_event' => $event,
             'is_registered' => $is_registered,
             'shifts_with_quota' => $shiftWithQuota
         ]);
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -97,6 +97,30 @@
                     </div>
                 </div>
             </div>
+            <div class="col-lg-4">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row align-items-start">
+                                    <div class="col-8">
+                                        <h5 class="card-title mb-9 fw-semibold"> Jumlah Event </h5>
+                                        <h4 class="fw-semibold mb-3">{{ $count_event }}</h4>
+                                    </div>
+                                    <div class="col-4">
+                                        <div class="d-flex justify-content-end">
+                                            <div
+                                                class="text-white bg-warning rounded-circle p-6 d-flex align-items-center justify-content-center">
+                                                <i class="ti ti-calendar-event fs-6"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     @else
         @if ($is_registered)
