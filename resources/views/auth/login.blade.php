960e70d4157d5df5e74498e7fddedc93465cba27
tcandra24 <tcandra007@gmail.com>
2024-03-08 09:28:50 +0700

    Rev. Login Page for admin

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -54,16 +54,16 @@
             </div>
         </div>
         <button type="submit" class="btn btn-primary w-100 py-8 fs-4 mb-2 rounded-2">Login</button>
-        <div class="d-flex align-items-center justify-content-center">
+        {{-- <div class="d-flex align-items-center justify-content-center">
             <p class="fs-4 mb-2 fw-bold">Atau</p>
-        </div>
+        </div> --}}
         {{-- <a href="/auth/google" class="btn btn-secondary w-100 py-8 fs-4 mb-2 rounded-2">
             Login dengan Google
         </a> --}}
-        <div class="d-flex align-items-center justify-content-center">
+        {{-- <div class="d-flex align-items-center justify-content-center">
             <p class="fs-4 mb-0 fw-bold">Belum punya akun?</p>
             <a class="text-primary fw-bold ms-2"
                 href="{{ route('register', ['event' => request()->get('event')]) }}">Register</a>
-        </div>
+        </div> --}}
     </form>
 @endsection
