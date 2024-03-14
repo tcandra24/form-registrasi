552c7f2f8820b2245b770de43a68f1596599ba83
tcandra24 <tcandra007@gmail.com>
2024-03-14 10:48:36 +0700

    Rev. Link Banner

--- resources/views/link/index.blade.php
+++ resources/views/link/index.blade.php
@@ -6,9 +6,10 @@
 
 @section('content')
     <div class="card-body">
-        <a href="{{ $event->link }}" class="position-relative">
+        <a href="{{ route('register', ['event' => $event->id]) }}" class="position-relative">
             <div class="row w-100 h-100 position-absolute top-0 start-0 d-flex justify-content-center ">
-                <button class="btn btn-warning btn-lg w-50 animate-bounce fw-bold"> KLIK DISINI </button>
+                <button class="btn btn-warning btn-lg w-50 animate-bounce fw-bold" style="opacity: 0.8;"> KLIK DISINI
+                </button>
             </div>
             <img class="card-img-top " src="{{ $event->image }}" alt="{{ $event->slug }}">
         </a>
