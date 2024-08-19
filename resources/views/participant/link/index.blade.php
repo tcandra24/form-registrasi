9605fca1ccdf622e890cd7480ee1cdb49a788906
tcandra24 <tcandra007@gmail.com>
2024-08-19 13:42:36 +0700

    Rev Link to Login

--- resources/views/participant/link/index.blade.php
+++ resources/views/participant/link/index.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
     <div class="card-body">
-        <a href="{{ route('register.participant') }}" class="position-relative">
+        <a href="{{ route('login.participant') }}" class="position-relative">
             <div class="row w-100 h-100 position-absolute top-0 start-0 d-flex justify-content-center ">
                 <button class="btn btn-warning btn-lg w-50 animate-bounce fw-bold" style="opacity: 0.8;"> KLIK DISINI
                 </button>
