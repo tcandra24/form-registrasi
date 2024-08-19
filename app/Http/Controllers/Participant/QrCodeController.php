6e17533d27961cb972491f200d8808790657515a
tcandra24 <tcandra007@gmail.com>
2024-08-19 13:38:23 +0700

    Add No Registration

--- app/Http/Controllers/Participant/QrCodeController.php
+++ app/Http/Controllers/Participant/QrCodeController.php
@@ -43,6 +43,11 @@ class QrCodeController extends Controller
         $registration = app($event->model_path)->where('registration_number', $no_registration)->first();
 
         $fields = collect([]);
+        $objectStd = new \stdClass();
+
+        $objectStd->title = 'Nomer Registrasi';
+        $objectStd->value = $no_registration;
+        $fields->push($objectStd);
 
         foreach($forms as $form){
             $value = null;
@@ -56,7 +61,6 @@ class QrCodeController extends Controller
                 $value = $registration->{$form->name};
             }
 
-            $objectStd = new \stdClass();
             $objectStd->title = $form->label;
             $objectStd->value = $value;
 
